0d13b35f9cb56887955cf864d1fce2573320454a
Rakesh Chittineni <rakeshch.net@gmail.com>
2021-02-05 16:03:16 -0500

    adding competed files

---
+++ src/containers/Persons.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actionTypes from '../store/actions';
+
+import Person from '../components/Person/Person';
+import AddPerson from '../components/AddPerson/AddPerson';
+
+class Persons extends Component {
+
+    // personAddedHandler = () => {
+    //     const newPerson = {
+    //         id: Math.random(), // not really unique but good enough here!
+    //         name: 'Max',
+    //         age: Math.floor( Math.random() * 40 )
+    //     }
+    //     this.setState( ( prevState ) => {
+    //         return { persons: prevState.persons.concat(newPerson)}
+    //     } );
+    // }
+
+    // personDeletedHandler = (personId) => {
+    //     this.setState( ( prevState ) => {
+    //         return { persons: prevState.persons.filter(person => person.id !== personId)}
+    //     } );
+    // }
+
+    render () {
+        return (
+            <div>
+                <AddPerson personAdded={this.props.onPersonAdded} />
+                {this.props.storedPersons.map(person => (
+                    <Person 
+                        key={person.id}
+                        name={person.name} 
+                        age={person.age} 
+                        clicked={() => this.props.onPersonDeleted(person.id)}/>
+                        //clicked={() => this.personDeletedHandler(person.id)}/>
+                ))}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps=state=>{
+    return {
+        storedPersons: state.prsns.persons
+    };
+};
+
+const mapDispatchToProps = dispatch =>{
+    return {
+        onPersonAdded:() => dispatch({ type: actionTypes.ADD }),
+        onPersonDeleted:(id) => dispatch({ type:actionTypes.DELETE, value: id })
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
